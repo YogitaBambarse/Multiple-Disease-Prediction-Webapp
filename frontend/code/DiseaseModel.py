b057219acff7d269b624de8d31284282e6685b41
Yogita bambarse <yogitabambarse412@gmail.com>
2026-03-30 18:30:22 +0530

    Refactor data path handling in DiseaseModel
    Refactor file paths to use a consistent data path and improve code readability.

--- frontend/code/DiseaseModel.py
+++ frontend/code/DiseaseModel.py
@@ -1,6 +1,7 @@
 import xgboost as xgb
 import pandas as pd
 import os
+
 class DiseaseModel:
 
     def __init__(self):
@@ -8,8 +9,12 @@ class DiseaseModel:
         self.symptoms = None
         self.pred_disease = None
         self.model = xgb.XGBClassifier()
+
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.data_path = os.path.join(BASE_DIR, '..', 'data')
+
         self.diseases = self.disease_list()
-        
+
     def load_xgboost(self, model_path):
         self.model.load_model(model_path)
 
@@ -20,61 +25,54 @@ class DiseaseModel:
         self.symptoms = X
         disease_pred_idx = self.model.predict(self.symptoms)
         self.pred_disease = self.diseases[disease_pred_idx].values[0]
+
         disease_probability_array = self.model.predict_proba(self.symptoms)
         disease_probability = disease_probability_array[0, disease_pred_idx[0]]
+
         return self.pred_disease, disease_probability
 
-    
     def describe_disease(self, disease_name):
-
         if disease_name not in self.diseases:
             return "That disease is not contemplated in this model"
-        
-        # Read disease dataframe
-        desc_df = pd.read_csv(os.path.join(BASE_DIR, '..', 'data', 'symptom_Description.csv'))
+
+        file_path = os.path.join(self.data_path, 'symptom_Description.csv')
+        desc_df = pd.read_csv(file_path)
         desc_df = desc_df.apply(lambda col: col.str.strip())
 
         return desc_df[desc_df['Disease'] == disease_name]['Description'].values[0]
 
     def describe_predicted_disease(self):
-
         if self.pred_disease is None:
             return "No predicted disease yet"
 
         return self.describe_disease(self.pred_disease)
-    
-    def disease_precautions(self, disease_name):
 
+    def disease_precautions(self, disease_name):
         if disease_name not in self.diseases:
             return "That disease is not contemplated in this model"
 
-        # Read precautions dataframe
-        prec_df = pd.read_csv(os.path.join(BASE_DIR, '..', 'data', 'symptom_precaution.csv'))
+        file_path = os.path.join(self.data_path, 'symptom_precaution.csv')
+        prec_df = pd.read_csv(file_path)
         prec_df = prec_df.apply(lambda col: col.str.strip())
 
         return prec_df[prec_df['Disease'] == disease_name].filter(regex='Precaution').values.tolist()[0]
 
     def predicted_disease_precautions(self):
-
         if self.pred_disease is None:
             return "No predicted disease yet"
 
         return self.disease_precautions(self.pred_disease)
 
-    def disease_list(self, kaggle_dataset):
+    def disease_list(self):
+        file_path = os.path.join(self.data_path, 'clean_dataset.tsv')
 
-      BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-      file_path = os.path.join(BASE_DIR, '..', 'data', 'clean_dataset.tsv')
-          df = pd.read_csv(file_path, sep='\t')
-       
-        
-        # Preprocessing
-        y_data = df.iloc[:,-1]
-        X_data = df.iloc[:,:-1]
+        df = pd.read_csv(file_path, sep='\t')
+
+        y_data = df.iloc[:, -1]
+        X_data = df.iloc[:, :-1]
 
         self.all_symptoms = X_data.columns
 
-        # Convert y to categorical values
         y_data = y_data.astype('category')
-        
+
         return y_data.cat.categories
